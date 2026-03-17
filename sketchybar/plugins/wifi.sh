f9278f0525e5cb30396516a492c8d7549a059947
luke <onegqluke@icloud.com>
2026-03-17 00:03:42 -0400

    Changed border color - will fix wifi and others later

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,14 +3,16 @@
 PLUGIN_DIR="$HOME/.config/sketchybar/plugins"
 . $PLUGIN_DIR/colors.sh
 
-SSID="$(system_profiler SPAirPortDataType | awk '/Current Network Information:/ { getline; print substr($0, 13, (length($0) - 13)); exit }')"
+CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
 if [ "$SSID" = "" ]; then
 #   sketchybar --set $NAME label="Disconnected" icon=睊
-  sketchybar --set $NAME icon=󰖪 \
+  sketchybar --set $NAME icon=睊 \
                          icon.color=$RED
 else
 #   sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
-  sketchybar --set $NAME icon=󰖩 \
+  sketchybar --set $NAME icon=直 \
                          icon.color=$FGALT
 fi
